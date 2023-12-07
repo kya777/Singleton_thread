fe6d3fc16b9f16edfcc434eec852bafb39f836eb
kya777 <52593557+kya777@users.noreply.github.com>
2023-12-07 20:12:03 +0900

    thread thread1(&a, std::ref(mu));

--- Singleton/Main.cpp
+++ Singleton/Main.cpp
@@ -22,20 +22,17 @@ int main()
 	CSingleton& temp = CSingleton::GetObj();
 
 	mutex mu;
-	int d = 0;
 
+	// 하나의 mutex로 관리해야 함
+	// thread가 2개라고 2개의 mutex로 잠구면 잠겨있는지 몰라서 레이스컨디션이 발생함.
+	// 데드락을 피하기 위해 std::lock_gard를 사용함.
 	thread thread1(&a, std::ref(mu));
 	thread thread2(&a, std::ref(mu));
-
-	
-
 
 	thread1.join();
 	thread2.join();
 
-
 	std::cout << temp.GetObj().GetTestNum() << std::endl;
-
 
 	return 0;
 }
